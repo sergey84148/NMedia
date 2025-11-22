415a12a5ba7c1acb053d62059a3b8be79ef417c6
sergey84148 <sergey84145@gmaile.com>
2025-11-22 17:50:13 +0300

    update

--- app/src/main/java/ru/netology/nmedia/dto/Post.kt
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -4,8 +4,8 @@ data class Post(
     val author: String,
     val content: String,
     val published: String,
-    var likes: Int,
-    var shares: Int,
-    var views: Int,
-    var likedByMe: Boolean
+    val likes: Int,
+    val shares: Int,
+    val views: Int,
+    val likedByMe: Boolean
 )
